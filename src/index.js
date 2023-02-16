686b4c7beced5c631fbef619a77eaa27291c2802
SashaKrynytska <krynytskaya@gmail.com>
2023-02-16 23:59:12 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-
-// import { Provider } from 'react-redux';
-// import { store } from 'redux/store';
-
-// import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// );
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
