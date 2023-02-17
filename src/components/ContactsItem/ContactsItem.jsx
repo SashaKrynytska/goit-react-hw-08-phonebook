8255e196f28d27e15aeabb818749859b43651125
SashaKrynytska <krynytskaya@gmail.com>
2023-02-17 18:23:08 +0200

    small fixes

--- src/components/ContactsItem/ContactsItem.jsx
+++ src/components/ContactsItem/ContactsItem.jsx
@@ -1,15 +1,14 @@
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contacts/contactsOperations';
-import { Button } from "components/Button/Button";
+import { Button } from 'components/Button/Button';
 
 import PropTypes from 'prop-types';
-import { Item } from "./ContactsItem.styled";
 
 const ContactsItem = ({ contact: { id, name, number } }) => {
   const dispatch = useDispatch();
 
   return (
-    <Item key={id}>
+    <li key={id}>
       <span>{name}</span>
       <span>{number}</span>
       <Button
@@ -17,7 +16,7 @@ const ContactsItem = ({ contact: { id, name, number } }) => {
         title={'Delete'}
         onClick={() => dispatch(deleteContact(id))}
       />
-    </Item>
+    </li>
   );
 };
 
